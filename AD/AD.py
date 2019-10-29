064dcced1dc27a2c05a397fe5401c48b13d6fd28
assaf <assafduek@gmail.com>
2019-10-28 20:29:44 -0400

    fixed

--- AD/AD.py
+++ AD/AD.py
@@ -2,8 +2,9 @@
 import boto3
 
 if __name__ == '__main__':
+
+
     session =boto3.Session(profile_name='AD')
-    ec2=Session.resource('ec2')
+    ec2 = session.resource('ec2')
     for i in ec2.instances.all():
         print(i)
-    
